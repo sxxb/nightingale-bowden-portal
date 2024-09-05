2086d36d7d33a099706eb3585182e993a929b7b8
sxxb <scottbelcher@live.com.au>
2024-09-05 15:32:25 +0930

    addressing further base/link issues - working on dev server

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://sxxb.github.io',
-    base: '/nightingale-bowden-portal'
+    base: '/nightingale-bowden-portal/'
 });
